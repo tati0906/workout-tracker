2c8eb55e2ffab14defc29d396ec9def7c1fd57b6
Tatiana V <tatianavanegas2006@gmail.com>
2025-10-02 23:19:49 -0500

    Espacios del index para manejo de errores

---
+++ src/routes/v1/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const usersRoutes = require('./users.routes');
+
+
+
+
+
+
+
+
+
+router.use('/users', usersRoutes);
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
